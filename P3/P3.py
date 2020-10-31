a7816c6d0a9b8d037b039956f47c1acf7f53cf06
ch3coch3 <020611j87208x@gmail.com>
2020-10-31 19:50:50 +0800

    begin P3

---
+++ P3/P3.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import Pillow as image
+
+
+def conv2d(ori,kernal):
+    ori = np.pad(ori,1,'constant',constant_values = 0)
+    dimension = ori.shape
+    result = np.zeros((250,461))
+    for i in range(0,250):
+        for j in range(0,461):
+            ori_1 = ori[0+i:3+i,0+j:3+j]
+            ori_2 = ori_1*kernal
+            ori_2 = ori_2.sum()/kernal.sum()
+            result[i,j] = int(ori_2)
+    return result
+
+img = cv2.imread("opencv_dl_hw\Dataset_opencvdl\Q3_Image\Chihiro.jpg")
+# filter
+x,y = np.mgrid[-1:2, -1:2]
+gaussian_kernal = np.exp(-(x**2 + y**2))
+gaussian_kernal = gaussian_kernal / gaussian_kernal.sum()
+
+# tranfer to gray scale
+gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+gaussian = conv2d(gray,gaussian_kernal)
+print(gray)
+print(gaussian)
+# convolution
+gaussian = image.formarray(gaussian,'gray')
+cv2.imshow("gray",gray)
+cv2.imshow("gaussian",gaussian)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
